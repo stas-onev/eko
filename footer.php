afbf6982fc6989cc107dcef2d570b4d1c0b156e4
Stas Goshovsky <stasgo@one-v.co.il>
2019-03-27 12:11:52 +0200

    - 15% panel

--- footer.php
+++ footer.php
@@ -23,8 +23,10 @@
 					<div class="panel-form">
 						<form action="#" class="form">
 							<h4>הנחה שתתלבש עלייך טוב</h4>
-							<div class="form-text">
-								<p>ממלאים את הפרטים בטופס ומקבלים באותו רגע <br>מייל והודעה עם שובר של 10% הנחה למימוש מיידי <br>בחנות האונליין <span>EKO</span> על מגוון פחי</p>
+							<div class="form-content">
+								<div class="form-text">
+									<p>ממלאים את הפרטים בטופס ומקבלים באותו רגע <br>מייל והודעה עם שובר של 10% הנחה למימוש מיידי <br>בחנות האונליין <span>EKO</span> על מגוון פחי</p>
+								</div>
 							</div>
 							<div class="form-fields">
 								<div class="form-row">
@@ -43,7 +45,120 @@
 									</div>
 								</div>
 							</div>
-							<button class="form-submit" type="submit">
+							<button class="form-button" type="submit">
+								<span>שלחו לי את ההנחה</span>
+							</button>
+							<div class="checkbox">
+								<input type="checkbox" id="newsletter-10-percent">
+								<label for="newsletter-10-percent">ברור שאני רוצה לקבל מידע על מבצעים ומוצרים נוספים</label>
+							</div>
+						</form>
+					</div>
+					<div class="panel-close">אני רוצה הנחה אחרת</div>
+					<div class="panel-image">
+						<img src="img/apple.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="panel wine" id="panel-15-percent">
+			<div class="panel-inner">
+				<div class="side-text">
+					<div class="sale-line no-bg">
+						<div class="line-content">Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span></div>
+					</div>
+				</div>
+				<div class="panel-content">
+					<div class="panel-discount">
+						<div class="discount-content">
+							<strong>15%</strong>
+							<span>הנחה</span>
+						</div>
+					</div>
+					<div class="panel-form">
+						<form action="#" class="form">
+							<h4>משתפים ומרוויחים</h4>
+							<div class="form-content">
+								<div class="form-text">
+									<p>1. לוחצים על כפתור השיתוף לפייסבוק ו...משתפים. <br>2. ממלאים את הפרטים בטופס ומקבלים באותו רגע מייל והודעה עם שובר של 15% הנחה למימוש מיידי על מגוון פחי <span>EKO</span> בחנות האונליין </p>
+								</div>
+								<button class="form-button" type="submit">
+									<span>קליק לשיתוף בפייס</span>
+								</button>
+							</div>
+							<div class="form-fields">
+								<div class="form-row">
+									<div class="input-field disabled">
+										<input disabled type="text" placeholder="שם מלא"><!-- Name -->
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="input-field disabled">
+										<input disabled type="tel" placeholder="טלפון כולל קידומת"><!-- Phone -->
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="input-field disabled">
+										<input disabled type="email" placeholder="מייל"><!-- Email -->
+									</div>
+								</div>
+							</div>
+							<button class="form-button" disabled type="submit">
+								<span>שלחו לי את ההנחה</span>
+							</button>
+							<div class="checkbox">
+								<input type="checkbox" disabled id="newsletter-10-percent">
+								<label for="newsletter-10-percent">ברור שאני רוצה לקבל מידע על מבצעים ומוצרים נוספים</label>
+							</div>
+						</form>
+					</div>
+					<div class="panel-close">אני רוצה הנחה אחרת</div>
+					<div class="panel-image">
+						<img src="img/apple.png" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="panel gray" id="panel-20-percent">
+			<div class="panel-inner">
+				<div class="side-text">
+					<div class="sale-line no-bg">
+						<div class="line-content">Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span>Sale<span>Sale</span></div>
+					</div>
+				</div>
+				<div class="panel-content">
+					<div class="panel-discount">
+						<div class="discount-content">
+							<strong>20%</strong>
+							<span>הנחה</span>
+						</div>
+					</div>
+					<div class="panel-form">
+						<form action="#" class="form">
+							<h4>הנחה שתתלבש עלייך טוב</h4>
+							<div class="form-content">
+								<div class="form-text">
+									<p>ממלאים את הפרטים בטופס ומקבלים באותו רגע <br>מייל והודעה עם שובר של 10% הנחה למימוש מיידי <br>בחנות האונליין <span>EKO</span> על מגוון פחי</p>
+								</div>
+							</div>
+							<div class="form-fields">
+								<div class="form-row">
+									<div class="input-field">
+										<input type="text" placeholder="שם מלא"><!-- Name -->
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="input-field">
+										<input type="tel" placeholder="טלפון כולל קידומת"><!-- Phone -->
+									</div>
+								</div>
+								<div class="form-row">
+									<div class="input-field">
+										<input type="email" placeholder="מייל"><!-- Email -->
+									</div>
+								</div>
+							</div>
+							<button class="form-button" type="submit">
 								<span>שלחו לי את ההנחה</span>
 							</button>
 							<div class="checkbox">
